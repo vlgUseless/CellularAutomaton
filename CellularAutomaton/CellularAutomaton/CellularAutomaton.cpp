e825eadd42bdd732980412b22fb31473e56df6a3
Vladislav Gaar <gaarvlad@gmail.com>
2024-11-21 21:54:59 +0300

    Minor changes and fixed toroidal boundary conditions

--- CellularAutomaton/CellularAutomaton/CellularAutomaton.cpp
+++ CellularAutomaton/CellularAutomaton/CellularAutomaton.cpp
@@ -5,10 +5,10 @@
 #include <thread>
 
 CellularAutomaton::CellularAutomaton(int width, int height, std::string rule, std::string boundaryCondition, bool randomInit)
-	: width(width), height(width), rule(rule), boundaryCondition(boundaryCondition) {
+	: width(width), height(height), rule(rule), boundaryCondition(boundaryCondition) {
 
-	field.resize(width, std::vector<int>(height, 0));
-	nextField.resize(width, std::vector<int>(height, 0));
+	field.resize(height, std::vector<int>(width, 0));
+	nextField.resize(height, std::vector<int>(width, 0));
 
 	if (randomInit) {
 		generateInitialState();
@@ -22,8 +22,8 @@ void CellularAutomaton::generateInitialState() {
 	rng.seed(time(0));
 	std::uniform_int_distribution<int> dist(0, 1);
 
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
+	for (int x = 0; x < height; x++) {
+		for (int y = 0; y < width; y++) {
 			field[x][y] = dist(rng);
 		}
 	}
@@ -33,8 +33,8 @@ void CellularAutomaton::setInitialState() {
 	std::cout << "Enter the initial state of the field (" << width << " x " << height << ").\n";
 	std::cout << "Enter 0 for dead cell and 1 for alive.\n";
 
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
+	for (int x = 0; x < height; x++) {
+		for (int y = 0; y < width; y++) {
 			int cellValue;
 			while(true) {
 				std::cin >> cellValue;
@@ -58,8 +58,8 @@ void CellularAutomaton::setInitialState() {
 
 void CellularAutomaton::evolve() {
 	// Проходим по каждой клетке
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
+	for (int x = 0; x < height; x++) {
+		for (int y = 0; y < width; y++) {
 			// Подсчитываем количество живых соседей для клетки (x, y)
 			int aliveNeighbors = countAliveNeighbors(x, y);
 
@@ -76,8 +76,8 @@ void CellularAutomaton::evolve() {
 	}
 
 	// Копируем nextField в field для следующего поколения
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
+	for (int x = 0; x < height; x++) {
+		for (int y = 0; y < width; y++) {
 			field[x][y] = nextField[x][y];
 		}
 	}
@@ -87,9 +87,9 @@ void CellularAutomaton::display() const{
 	using namespace std::this_thread; // sleep_for, sleep_until
 	using namespace std::chrono; // nanoseconds, system_clock, seconds
 
-	for (int x = 0; x < width; x++) {
+	for (int x = 0; x < height; x++) {
 		std::string row = "";
-		for (int y = 0; y < height; y++) {
+		for (int y = 0; y < width; y++) {
 			// Используем символы для живой и мёртвой клетки
 			row.append(field[x][y] == 1 ? "▮" : "▯");
 		}
@@ -104,10 +104,10 @@ void CellularAutomaton::display() const{
 
 
 unsigned int CellularAutomaton::countAliveNeighbors(int x, int y) const {
-	unsigned int s1 = getCell(x, y - 1);
-	unsigned int s2 = getCell(x, y + 1);
-	unsigned int s3 = getCell(x - 1, y);
-	unsigned int s4 = getCell(x + 1, y);
+	unsigned int s1 = getCell(x - 1, y);
+	unsigned int s2 = getCell(x + 1, y);
+	unsigned int s3 = getCell(x, y - 1);
+	unsigned int s4 = getCell(x, y + 1);
 
 	return (s1 << 3) + (s2 << 2) + (s3 << 1) + s4;
 }
@@ -115,23 +115,29 @@ unsigned int CellularAutomaton::countAliveNeighbors(int x, int y) const {
 
 int CellularAutomaton::getCell(int x, int y) const {
 	if (boundaryCondition == "toroidal") {
-		x = (x + width) % width;
-		y = (y + height) % height;
-		if (x < 0 || x >= width || y < 0 || y >= height) {
-			return field[y][x];
+		if (x < 0) {
+			return field[height - 1][y];
 		}
-		else {
-			return field[x][y];
+		else if(x > height) {
+			return field[0][y];
 		}
+
+		if (y < 0) {
+			return field[x][width-1];
+		}
+		else if (y > width) {
+			return field[x][0];
+		}
+		return field[x%height][y%width];
 	}
 	else if (boundaryCondition == "zero") {
-		if (x < 0 || x >= width || y < 0 || y >= height) {
+		if (x < 0 || x >= height || y < 0 || y >= width) {
 			return 0;
 		}
 		return field[x][y];
 	}
 	else if (boundaryCondition == "one") {
-		if (x < 0 || x >= width || y < 0 || y >= height) {
+		if (x < 0 || x >= height || y < 0 || y >= width) {
 			return 1;
 		}
 		return field[x][y];
